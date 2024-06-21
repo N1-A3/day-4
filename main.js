0693150599af42eecdc589c718cdfbdca4d7e341
Areeba155 <naz400617@gmail.com>
2024-06-21 23:04:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+
+let names: string[] =["areeba","eisha","nimra","maryam"];
+for(let i =1; i<names.length; i++){
+    console.log(names[i]);
+}
+    */
+var names = ["areeba", "eisha", "nimra"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log('hello ${name},would you like to learn some typescript today?');
+}
